97fd2dc0e85b33e250d1b2bb21ba9c44cf2666c5
ditosatrio87-arch <ditosatrio87@gmail.com>
2026-04-02 10:51:55 +0700

    hahaha

--- src/app/pages/admin/DocumentVerification.jsx
+++ src/app/pages/admin/DocumentVerification.jsx
@@ -67,7 +67,7 @@ export function DocumentVerification() {
 
     const { error } = await supabase.storage
       .from('document-files')
-      .upload(documents/${fileName}, file);
+      .upload(`documents/${fileName}, file);
 
     if (error) {
       console.error(error);
